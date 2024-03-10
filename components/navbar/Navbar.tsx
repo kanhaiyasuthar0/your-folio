bf07056992e7abc614d817da4f6405f881d48bd9
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2024-03-10 18:10:22 +0530

    ADD: added navbar fgor mobile screen

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
     <nav className="bg-gray-900 text-white shadow-lg">
       <div className="container mx-auto flex justify-between items-center p-4">
@@ -56,13 +59,35 @@ const Navbar = () => {
         </div>
         {/* Mobile menu button */}
         <div className="md:hidden flex items-center">
-          <button className="outline-none mobile-menu-button">
+          {/* <button className="outline-none mobile-menu-button">
+            SVG for mobile menu icon
+          </button> */}
+          <button
+            className="outline-none mobile-menu-button"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            aria-controls="mobile-menu"
+          >
             {/* SVG for mobile menu icon */}
+            {/* Add an SVG icon for the menu or use a text like 'Menu' as a placeholder */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
           </button>
         </div>
       </div>
       {/* Mobile menu */}
-      <div className="hidden mobile-menu">
+      <div className={`mobile-menu ${isMobileMenuOpen ? "block" : "hidden"}`}>
         <ul>
           <li>
             <Link href="/">
@@ -113,7 +138,7 @@ const Navbar = () => {
               </span>
             </Link>
           </li>
-          <li>
+          <li className="p-2">
             <SignedOut>
               <SignInButton />
             </SignedOut>
