25958b5e5c407c913d780106d81b19ad99447cb0
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2024-03-17 16:40:08 +0530

    added some good ui changes

--- app/(ui)/payment/page.tsx
+++ app/(ui)/payment/page.tsx
@@ -6,6 +6,9 @@ import {
   Elements,
   useStripe,
   useElements,
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement,
 } from "@stripe/react-stripe-js";
 import { Chela_One } from "next/font/google";
 
@@ -13,6 +16,24 @@ import { Chela_One } from "next/font/google";
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 );
+
+const cardElementOptions = {
+  style: {
+    base: {
+      color: "#32325d",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  },
+};
 // console.log(stripePromise, "stripePromise");
 
 const CheckoutForm = () => {
@@ -34,28 +55,76 @@ const CheckoutForm = () => {
 
     const cardElement = elements.getElement(CardElement)!;
 
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      //   type: "card",
-      //   card: cardElement,
-      type: "card",
-      card: cardElement,
+    // const { error, paymentMethod } = await stripe.({
+    //   //   type: "card",
+    //   //   card: cardElement,
+    //   type: "card",
+    //   card: cardElement,
+    // });
+
+    const response = await fetch("/api/payment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Pass the amount here; consider validating/setting this server-side as well
+      body: JSON.stringify({ amount: 9900 }), // Example for ₹99
     });
+    const { clientSecret } = await response.json();
+    console.log("🚀 ~ handleSubmit ~ clientSecret:", clientSecret);
+    // console.log(response, response.clientSecret, "clientSecret");
+    // let clientSecret = "";
+
+    const { error, paymentIntent } = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: cardElement,
+          billing_details: {
+            name: "Jenny Rosen",
+            address: {
+              city: "mumbai",
+              country: "in",
+              line1: "12",
+              line2: "1a",
+              postal_code: "12134",
+              state: "mh",
+            },
+            email: "kanhaiyasuthar0@gmail.com",
+            phone: "897689231",
+          },
+        },
+      }
+    );
 
     if (error) {
       console.error(error);
       setLoading(false);
     } else {
-      console.log("PaymentMethod", paymentMethod);
-      // Here you would call your API to create a payment intent
-      // and finalize the payment process
+      console.log("Payment successful", paymentIntent);
+      // Handle successful payment here
       setLoading(false);
     }
   };
 
   return (
     <form>
-      <CardElement />
-      <button onClick={handleSubmit}>Pay</button>
+      <label>
+        Card number
+        <CardNumberElement options={cardElementOptions} />
+      </label>
+      <label>
+        Expiration date
+        <CardExpiryElement options={cardElementOptions} />
+      </label>
+      <label>
+        CVC
+        <CardCvcElement options={cardElementOptions} />
+      </label>
+      <button onClick={handleSubmit}>
+        {" "}
+        {loading ? "loading..." : "Pay 99"}
+      </button>
     </form>
   );
 };
--- components/generics/ThreeDPin.tsx
+++ components/generics/ThreeDPin.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { PinContainer } from "../ui/3d-pin";
+import Link from "next/link";
 
 export function AnimatedPinDemo(props: any) {
   return (
@@ -8,25 +9,27 @@ export function AnimatedPinDemo(props: any) {
       className="h-[35rem] flex items-center justify-center"
       key={props.plan.name}
     >
-      <PinContainer
-        title={`your-folio@ ₹${props.plan.price}`}
-        href="www.google.com"
-      >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[30rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            {props.plan.name}
-          </h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">₹{props.plan.price}</span>
+      <Link href={"/payment"}>
+        <PinContainer
+          title={`your-folio@ ₹${props.plan.price}`}
+          href="www.google.com"
+        >
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[30rem] ">
+            <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+              {props.plan.name}
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500 ">₹{props.plan.price}</span>
+            </div>
+            {props.plan.features.map((feature: string, index: number) => (
+              <li key={index} className="text-sm mb-2">
+                ✓ {feature}
+              </li>
+            ))}
+            <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
           </div>
-          {props.plan.features.map((feature: string, index: number) => (
-            <li key={index} className="text-sm mb-2">
-              ✓ {feature}
-            </li>
-          ))}
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-        </div>
-      </PinContainer>
+        </PinContainer>
+      </Link>
     </div>
   );
 }
