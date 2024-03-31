4fc5929456fbda19b4c6a7ea478a6bbe75b57c23
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2024-03-31 11:17:37 +0530

    ADD: added docs for using yourfolio.in

--- app/(ui)/admin/folio/add/page.tsx
+++ app/(ui)/admin/folio/add/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 import { submitFolio } from "@/actions/submitFolio.action";
 import { currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 
 import React, { FormEvent } from "react";
 
-const AddFolio = async () => {
+const AddFolio = () => {
   async function submitAction(formData: FormData) {
     "use server";
     await submitFolio(formData);
