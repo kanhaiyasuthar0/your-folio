64fc3430aa985700c972c4a6a68d1fba2e6a3ace
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2024-04-01 00:44:37 +0530

    no banner for ind site and image fix

--- components/generics/HoliOfferBanner.tsx
+++ components/generics/HoliOfferBanner.tsx
@@ -1,17 +1,25 @@
-// components/HoliOfferBanner.js
+"use client";
+
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const HoliOfferBanner = () => {
-  return (
-    <div className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white font-semibold py-3 overflow-hidden">
-      <div className="flex justify-center items-center">
-        <div className="animate-marquee-holi whitespace-nowrap">
-          🎨 Holi Special: Enjoy colorful deals! Free services for the first 25
-          sign-ups! 🎨
+  const pathname = usePathname()!;
+  const regex = /^\/showcase\/folioUsers\/.*$/;
+  // const regex = /^\/showcase(?:\/folioUsers\/[^/]+\/.*)?$/;
+
+  if (!regex.test(pathname)) {
+    return (
+      <div className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white font-semibold py-3 overflow-hidden">
+        <div className="flex justify-center items-center">
+          <div className="animate-marquee-holi whitespace-nowrap">
+            🎨 Holi Special: Enjoy colorful deals! Free services for the first
+            25 sign-ups! 🎨
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default HoliOfferBanner;
--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
       "www.gravatar.com",
       "saisolutions.vercel.app",
       "img.clerk.com",
+      "content.jdmagicbox.com",
     ],
   },
 };
